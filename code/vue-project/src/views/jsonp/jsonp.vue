1a37983df446284e69a11e48ac3fb24d5e00a95b
Sinosaurus <shuiqing1991@gmail.com>
2019-06-08 13:17:24 +0800

    update vue-project

--- code/vue-project/src/views/jsonp/jsonp.vue
+++ code/vue-project/src/views/jsonp/jsonp.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <router-link :to="{name: 'home', params: {test: h}}">home</router-link>
-    <input type="text" v-model="h">
+    <!-- <input type="text" v-model="h"> -->
+    <br>
+    <p>原生输入框</p>
+    <input v-model="value" type="text" @input="setValue($event.target)">
+
+    <p>不适用v-model</p>
+    <input type="text" @input="setValueNotWidthVModel($event.target)">
+    <h3>组件化</h3>
+    <p>element-ui</p>
+    <el-input v-model="form.accout" @input="changeInputValue"></el-input>
+    <p>iview</p>
+    <Input v-model="form.accout" @input="changeInputValue" />
   </div>
 </template>
 
@@ -10,16 +21,53 @@ export default {
   data() {
     return {
       url: 'http://www.gov.cn/pushinfo/v150203/pushinfo.jsonp',
-      h: 111
+      h: 111,
+      value: '',
+      form: {
+        accout: 0,
+      }
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.form.accout = '92309239493'.replace(/(\d{4})(?=\d)/g, '$1 ')
+    }, 1500)
+  },
+  methods: {
+    changeInputValue (val) {
+      console.log(val)
+      // return
+      const v = val.toString().replace(/[^0-9]/ig,"")
+      console.log(v)
+      this.$nextTick(() => {
+        // https://segmentfault.com/q/1010000009840451/a-1020000010449110
+        this.form.accout = v.replace(/(\d{4})(?=\d)/g, '$1 ')
+      })
+    },
+    setValueNotWidthVModel (target) {
+      const v = target.value.toString().replace(/[^0-9]/ig,"")
+      this.form.accout = v.replace(/\s/g, '')
+      target.value = v.replace(/(\d{4})(?=\d)/g, '$1 ')
+    },
+    setValue (target) {
+      // https://blog.csdn.net/wangming520liwei/article/details/53037951
+      // https://blog.csdn.net/xutongbao/article/details/79287376
+      const v = target.value.toString().replace(/[^0-9]/ig,"")
+      console.log(v, 'old')
+
+      // console.log(v.replace('/\D/g', ''), 'new')
+      // console.log(v.replace(/[^0-9]/ig,""), 'new')
+      this.form.accout = v.replace(/\s/g, '')
+      this.value = v.replace(/(\d{4})(?=\d)/g, '$1 ')
     }
   },
   created() {
-    console.log(this.$jsonp)
-    this.$jsonp(this.url, { callbackQuery: 'callback', callbackName: 'pushInfoJsonpCallBack' }).then(res => {
-      console.log(res, 111111111111)
-    }).catch(err => {
-      console.log(err)
-    })
+    // console.log(this.$jsonp)
+    // this.$jsonp(this.url, { callbackQuery: 'callback', callbackName: 'pushInfoJsonpCallBack' }).then(res => {
+    //   console.log(res, 111111111111)
+    // }).catch(err => {
+    //   console.log(err)
+    // })
   }
 }
 </script>
---
+++ vue/vue -- input框输入数据实时修改.md
@@ -0,0 +1,88 @@
+# vue中如何实时修改输入的值
+经常看到需要对用户输入的值进行实时修改，有时是需要修改为指定的展示内容，有时候是用来校验，禁止用户输入非法数据，总之是一个常见的需求吧，只是自己一直没有特意去关注。思来想去还是有必要了解一下。
+
+## 操作方法
+一直听说各种方式，记得最深的便是利用`computed`的计算属性，通过`set` 和 `get` 来进行修改，其他的也有所见闻。先实现一种，再进行其他其他深究，以及使用好坏。
+
+> [vue文档说明](https://cn.vuejs.org/v2/guide/forms.html) *对于需要使用输入法 (如中文、日文、韩文等) 的语言，你会发现 v-model 不会在输入法组合文字过程中得到更新。如果你也想处理这个过程，请使用 `input` 事件。*
+
+> 需要分两种形式，一种是原生，一种是基于组件，假设需求为*输入数字，同时四位一个空格，类似输入银行卡号*
+
++ 原生
+> 使用 v-model 进行数据绑定
+```
+// template
+<input v-model="value" type="text" @input="setValue($event.target)">
+// js
+data () {
+    return {
+        value: ''
+    }
+}
+
+setValue (target) {
+    // 输入的数据进行初始化，将非数字的替换为空
+    const val = target.value.toString().replace(/[^0-9]/ig,"")
+    // 重新赋值
+    this.value = v.replace(/(\d{4})(?=\d)/g, '$1 ')
+}
+```
+
+![原始输入框](./../images/vue/input/primary.gif)
+
+> 不适用  v-model 进行数据绑定
+
+```
+// template
+<input type="text" @input=""></input>
+// js
+data () {
+    return {
+        form {
+            accout: ''
+        }
+    }
+}
+setValueNotWidthVModel (target) {
+    const v = target.value.toString().replace(/[^0-9]/ig,"")
+    // 此处是获取数据的地方
+    this.form.accout = v.replace(/\s/g, '')
+    // 此处是页面进行显示
+    target.value = v.replace(/(\d{4})(?=\d)/g, '$1 ')
+}
+```
+
+![原始输入框](./../images/vue/input/primary-not-model.gif)
+
++ 组件方式
+组件都是对原始`input`进行额外处理了一层
+
+```
+<!-- template -->
+// element-ui
+<el-input v-model="form.accout" @input="changeInputValue"></el-input>
+// iview
+<Input v-model="form.accout" @input="changeInputValue" />
+<!-- js -->
+data () {
+    return {
+        form: {
+            account: '0'
+        }
+    }
+}
+// 由于是组件，返回值 v 直接是value ，无法传参，或者`$event`
+changeInputValue (v) {
+    const v = val.toString().replace(/[^0-9]/ig,"")
+    // 需要延迟一下，对值进行设置 https://github.com/ElemeFE/element/blob/dev/packages/input/src/input.vue 可以看到设置原始值时，使用 this.$nextTick()
+    this.$nextTick(() => {
+        // https://segmentfault.com/q/1010000009840451/a-1020000010449110
+        this.form.accout = v.replace(/(\d{4})(?=\d)/g, '$1 ')
+    })
+}
+```
+
+![组件](./../images/vue/input/components.gif)
+
+## 总结
++ 依旧不知道使用`computed`是如何实现的，上述方法，通过`input`时间，便可以一直拿到值，不用额外进行处理，因为`v-model`进行了绑定
