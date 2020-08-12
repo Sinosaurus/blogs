a9bf096dd10de6d6991f86885545a9727f75aa71
Sinosaurus <shuiqing1991@gmail.com>
2020-08-12 22:39:03 +0800

    feate vueDate 添加 iviewTable

---
+++ code/vue-project/src/views/vueData/components/iviewTable.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Table @on-sort-change="handleSortChange" v-bind="$attrs" v-on="$listeners"></Table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    handleSortChange (column, key, order) {
+      console.log(column, key, order)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- code/vue-project/src/views/vueData/index.vue
+++ code/vue-project/src/views/vueData/index.vue
@@ -5,23 +5,63 @@
       <el-table-column sortable label="test" prop="test"></el-table-column>
     </baseTable>
     <baseInheritAttrs :data="tableData" />
+    <iviewTable :columns="columns1" :data="data1" />
   </div>
 </template>
 
 <script>
 import baseTable from './components/BaseTable'
 import baseInheritAttrs from './components/BaseInheritAttrs'
+import iviewTable from './components/iviewTable'
 export default {
   name: 'attrsListener',
   components: {
     baseTable,
     baseInheritAttrs,
+    iviewTable
   },
   data() {
     return {
       tableData: [
         {
           test: 'test',
+        },
+      ],
+      columns1: [
+        {
+          title: '姓名',
+          key: 'name',
+          sortable: true,
+        },
+        {
+          title: '年龄',
+          key: 'age',
+        },
+        {
+          title: '地址',
+          key: 'address',
+        },
+      ],
+      data1: [
+        {
+          name: '王小明',
+          age: 18,
+          address: '北京市朝阳区芍药居',
+        },
+        {
+          name: '张小刚',
+          age: 25,
+          address: '北京市海淀区西二旗',
+        },
+        {
+          name: '李小红',
+          age: 30,
+          address: '上海市浦东新区世纪大道',
+        },
+        {
+          name: '周小伟',
+          age: 26,
+          address: '深圳市南山区深南大道',
         },
       ],
     }
